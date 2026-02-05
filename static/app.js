baa04f845b6cc272fe50c90d8f70297e9a197b07
herman088 <hermancho08@gmail.com>
2026-02-06 07:09:28 +0800

    Implement Pop up modal to show metadata

--- static/app.js
+++ static/app.js
@@ -2,7 +2,7 @@ async function search() {
   const q = document.getElementById("search").value;
   const res = await fetch(`/search?dish=${encodeURIComponent(q)}`);
 
-  const data = await res.json();
+  const data = await res.json(); //response body to js object
 
   const container = document.getElementById("results");
   container.innerHTML = "";
@@ -11,6 +11,7 @@ async function search() {
     const div = document.createElement("div");
     div.className = "card";
     div.dataset.hawker = h.hawker;
+    div.dataset.meta = JSON.stringify(h); //js object to js string to be sent http,this case to be stored as data attr?keynamewillbestring
     div.innerHTML = `
       <img src = ${h.thumbnail}>
       <div class = "card-content">
@@ -27,4 +28,33 @@ document.getElementById("results").addEventListener("click", (e) => {
   if (!card) return;
 
   const hawker = card.dataset.hawker;
+  const meta = JSON.parse(card.dataset.meta); //JS string to js obj
+  openHawker(meta);
 });
+
+async function openHawker(cardData) {
+  document.getElementById("modal-image").src = cardData.thumbnail;
+  document.getElementById("modal-title").innerText = cardData.hawker;
+  document.getElementById("modal-rating").innerText =
+    `⭐ ${cardData.rating || "N/A"}`;
+  document.getElementById("mentions").innerText =
+    `Total mentions: ${cardData.mentions}`;
+  document.getElementById("reco-count").innerText =
+    `Total recommendations: ${cardData.recommended_mentions}`;
+  document.getElementById("positive-count").innerText =
+    `Total mentions: ${cardData.positive_mentions}`;
+
+  document.getElementById("modal").classList.remove("hidden");
+}
+
+/*
+document.getElementById("modal-close").addEventListener("click", () => {
+  document.getElementById("modal").classList.add("hidden");
+});
+
+document.getElementById("modal").addEventListener("click", (e) => {
+  if (e.target.id === "modal") {
+    document.getElementById("modal").classList.add("hidden");
+  }
+});
+*/
